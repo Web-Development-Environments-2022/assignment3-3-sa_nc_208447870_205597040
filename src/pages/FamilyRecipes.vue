bf87afcb297f6fb15a5e2de5f901b65000834f06
NirCohen1 <81625984+NirCohen1@users.noreply.github.com>
2022-08-18 12:52:36 +0300

    Add files via upload

---
+++ src/pages/FamilyRecipes.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default{
+    name:"family_recipes",
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
